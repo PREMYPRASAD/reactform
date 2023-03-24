7bbe2943a1f9b46621a624fbdd53d693f7da1a84
PREMYPRASAD <mailtopremy2010@gmail.com>
2023-03-24 13:56:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './components/Header';
+import Login from './components/Login';
+
+
+function App() {
+    return (
+        //jsx (javascript XML)
+        < div className="App" >
+<Header></Header>
+            <h1 >Welcome to react </h1>
+            <Login></Login>
+        </div >);
+}
+
+export default App; 
