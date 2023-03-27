eff9a60fd31b967cb68014c1639f8f08d3c0f6d7
PREMYPRASAD <mailtopremy2010@gmail.com>
2023-03-27 14:12:41 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 
-import './App.css';
-import Header from './components/Header';
-import Login from './components/Login';
+
+// import Header from './components/Header';
+// import Login from './components/Login';
+import { Component } from 'react';
+import TodoApp from './components/TodoApp/TodoApp';
 
 
-function App() {
-    return (
-        //jsx (javascript XML)
-        < div className="App" >
-<Header></Header>
-            <h1 >Welcome to react </h1>
-            <Login></Login>
-        </div >);
+// function App() {
+//     return (
+//         //jsx (javascript XML)
+//         < div className="App" >
+// <Header></Header>
+//             <h1 >Welcome to react </h1>
+//             <Login></Login>
+//         </div >)
+// }
+export class App extends Component {
+    render() {
+        return<div><TodoApp></TodoApp></div>
+    }
 }
-
 export default App; 
